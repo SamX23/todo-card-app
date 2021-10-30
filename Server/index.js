5c29e2edda7f9112e402da71b532b89b6e6d7f31
Samx23 <skalexsong@gmail.com>
2021-10-30 19:45:11 +0700

    Update : setup basic server

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,31 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express, { application } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const server = express();
+
+server.use("/posts", postRoutes);
+
+server.use(express.json({ limit: "30mb", extended: true }));
+server.use(express.urlencoded({ limit: "30mb", extended: true }));
+server.use(cors);
+
+// MONGODB ATLAS
+const CONNECTION_URL = "";
+const PORT = process.env.PORT || 3000;
+
+// mongoose
+//   .connect(CONNECTION_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() =>
+//     server.listen(PORT, () => console.log("SERVER RUNNING ON : ", PORT))
+//   )
+//   .catch((err) => console.log(err));
+
+// mongoose.set("useFindAndModify", false);
+
+server.listen(PORT, () => console.log("SERVER RUNNING ON : ", PORT));
