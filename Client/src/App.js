e6d7dc94be39419cb0df0aa90ecd3566a3d32c23
Samx23 <skalexsong@gmail.com>
2021-11-11 23:03:42 +0700

    Update : create api call

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,11 +2,18 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts";
 import memories from "./images/picture-1.jpg";
-
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
+import { useEffect } from "react";
 
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxwitdh="lg">
