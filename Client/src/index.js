99fa730d465c2d26e23b1ee8b3a3367a3d3f050d
Samx23 <skalexsong@gmail.com>
2021-11-11 23:03:06 +0700

    Update : create redux component

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 import App from "./App";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
