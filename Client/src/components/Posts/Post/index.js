7b69b0c639640e32fbc54d52e7ef4526868e9441
Samx23 <skalexsong@gmail.com>
2021-11-14 23:00:05 +0700

    Update : posts and post component condition

--- Client/src/components/Posts/Post/index.js
+++ Client/src/components/Posts/Post/index.js
@@ -1,7 +1,29 @@
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAltIcon";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
+
 import useStyles from "./styles";
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <div></div>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <Typography variant="h6">{post.creator}</Typography>
+    </Card>
+  );
 };
 
 export default Post;
